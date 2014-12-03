b174d796bd6132ed5ad18d12a48bab7d2ee3cd04
Jacob Bondley <hasharahha@gmail.com>
2014-12-03 08:37:18 -0500

    Update Gen.js

--- libaries/Gen.js
+++ libaries/Gen.js
@@ -1,9 +1,8 @@
 //$(element, attr, value, id)
-function $create(element, attr, value, id){
+function $create(element, attr, value{
   var Elem = document.createElement(element);
   Elem[attr] = value;
   document.body.appendChild(Elem);
-  Elem["id"] = id;
 }
 function $editprop(id, attr, value){
   var Cache = id;
