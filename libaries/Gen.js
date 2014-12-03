82875591fb5577c6dfcacac515aa82d94d944350
Jacob Bondley <hasharahha@gmail.com>
2014-12-03 07:33:03 -0500

    Update Gen.js

--- libaries/Gen.js
+++ libaries/Gen.js
@@ -1,5 +1,11 @@
 
 function $(selector){
 var self ={};
-
+var self.selecter = selector;
+self.element = document.querySelector(self.selector);
+self.setAttribute = function(name, value){
+  if(name){
+    self.element.setAttribute(name, value);
+  }
+}
 }
